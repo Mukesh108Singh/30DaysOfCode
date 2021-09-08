853b37ad27eeafb5a676f706b2e98aeb926e3936
Mukesh Singh <73029835+Mukesh108Singh@users.noreply.github.com>
2021-09-08 22:01:40 +0530

    Create FunctionInPython.py

---
+++ Day 19/FunctionInPython.py
@@ -0,0 +1,6 @@
+def formated_name(name1,name2):
+  formated_name1 = name1.title()
+  formated_name2 = name2.title()
+  return f" {formated_name1} and {formated_name2} are siblings"
+
+print(formated_name("raj","rohit"))
