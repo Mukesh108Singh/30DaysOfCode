d55d97d1c616a5fafe4144871e31274a767311d1
Mukesh Singh <73029835+Mukesh108Singh@users.noreply.github.com>
2021-09-08 22:09:15 +0530

    Update FunctionInPython.py

--- Day 19/FunctionInPython.py
+++ Day 19/FunctionInPython.py
@@ -1,6 +1,22 @@
+#Function With Outputs
+
 def formated_name(name1,name2):
   formated_name1 = name1.title()
   formated_name2 = name2.title()
   return f" {formated_name1} and {formated_name2} are siblings"
 
 print(formated_name("raj","rohit"))
+
+
+# @nd way
+
+
+def formated_name(name1,name2):
+  formated_name1 = name1.title()
+  formated_name2 = name2.title()
+  return f" {formated_name1} and {formated_name2} are siblings"
+
+myname = input("Enter ur Name \n")
+sibname = input("Enter ur sibling's Name \n")
+
+print(formated_name(myname,sibname))
